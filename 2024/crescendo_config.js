c93ab278a66df410f2c41c56b6a4ebb2dbdd0d2b
burmsrock <168002494+burmsrock@users.noreply.github.com>
2024-11-24 21:41:14 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -5,7 +5,7 @@ var config_data = `
   "page_title": "Crescendo 1502 WW",
   "checkboxAs": "10",
   "prematch": [
-    { "name": "Scouter Initials",
+    { "name": "Scouter First Name",
       "code": "s",
       "type": "scouter",
       "size": 12,
