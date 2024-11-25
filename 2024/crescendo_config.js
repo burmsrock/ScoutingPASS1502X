445dca7a71c04f5e34efa2f489afa968a2923e82
burmsrock <168002494+burmsrock@users.noreply.github.com>
2024-11-24 19:05:01 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -50,7 +50,7 @@ var config_data = `
       "required":"true"
     },
         { "name": "Predicition",
-      "code": "p",
+      "code": "as",
       "type": "predict",
       "choices": {
         "r": "Red",
@@ -64,14 +64,7 @@ var config_data = `
       "min": 1,
       "max": 99999
     },
-    { "name": "Auto Start Position",
-      "code": "as",
-      "type": "clickable_image",
-      "filename": "2024/field_image.png",
-      "clickRestriction": "one",
-      "allowableResponses": "1 12 13 24 25 36 37 48 49 60 61 72",
-      "shape": "circle 5 black red true"
-    }
+
   ],
   "auton": [
     { "name": "Leave Starting Zone",
