e4b81c399b4526eb1510185c11b6bbaa3c2740e3
burmsrock <168002494+burmsrock@users.noreply.github.com>
2024-11-24 21:49:45 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -2,7 +2,7 @@ var config_data = `
 {
   "dataFormat": "tsv",
   "title": "Scouting PASS 2024",
-  "page_title": "Crescendo 1502 Scouting",
+  "page_title": "Crescendo 1502",
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter First Name",
