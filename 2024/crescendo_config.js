7e89c2f5a4843a312801ba0cb4e1745a63451b01
burmsrock <168002494+burmsrock@users.noreply.github.com>
2025-01-04 22:03:09 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -58,7 +58,7 @@ var config_data = `
     { "name": "Auto Start Position",
       "code": "as",
       "type": "clickable_image",
-      "filename": "2024/field_image.png",
+      "filename": "2024/2025_Reefscape_Field.png",
       "clickRestriction": "one",
       "allowableResponses": "6 7 18 19 30 31 42 43 54 55 66 67",
       "shape": "circle 5 black red true"
